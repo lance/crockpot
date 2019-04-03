d53fd80f555158afbb5b80579f88e22203502c22
Lance Ball <lanceball@localhost.localdomain>
2019-04-02 21:16:46 -0400

    src: add structure for middleware inclusion

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 module.exports = exports = {
   Client: require('./lib/client'),
   LRA: require('./lib/lra'),
-  Participant: require('./lib/participant')
+  Participant: require('./lib/participant'),
+  Middleware: require('./lib/middleware')
 };
